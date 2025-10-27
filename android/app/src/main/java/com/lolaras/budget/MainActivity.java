c749a8dd677758f6f27e43445279d2600934784c
loleras94 <alexandris.alex@hotmail.com>
2025-10-27 14:11:15 +0000

    Add Capacitor Android project

---
+++ android/app/src/main/java/com/lolaras/budget/MainActivity.java
@@ -0,0 +1,5 @@
+package com.lolaras.budget;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lolaras.budget',
+  appName: 'Lolaras Budget',
+  webDir: 'dist'
+};
+
+export default config;
