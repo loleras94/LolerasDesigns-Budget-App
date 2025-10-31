9dab8675bf257aaf19688105f1ce6d217790ced3
loleras94 <loleras94@users.noreply.github.com>
2025-10-31 19:34:05 +0000

    Add TWA Android project

---
+++ twa/app/src/main/java/com/lolerasdesigns/budget/DelegationService.java
@@ -0,0 +1,14 @@
+package com.lolerasdesigns.budget;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
