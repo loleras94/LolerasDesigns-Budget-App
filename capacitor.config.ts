f130c1bd3e727c64fdacd632cc3da4d574b8322a
loleras94 <alexandris.alex@hotmail.com>
2025-10-31 08:07:48 +0200

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.lolaras.budget',
-  appName: 'Lolaras Budget',
-  webDir: 'dist'
-};
-
-export default config;
